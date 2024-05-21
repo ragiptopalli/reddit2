13f8e7d304dd5dc98be8d2f656109cc0b13f25c9
Ragip Topalli <ragiptopalli@gmail.com>
2024-05-21 03:18:54 +0200

    frontend: intial commit

---
+++ web/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className='flex flex-col items-center justify-between p-24'>
+      <h1>Hello, World</h1>
+      <Button>Click me! </Button>
+    </main>
+  );
+}
