4bef97b728c8dcb2933883d9a001d4e58e0d7cc6
Ragip Topalli <ragiptopalli@gmail.com>
2024-05-30 02:15:38 +0200

    chore🧹: cleaning duplicate folders

--- web/app/(main)/(user)/u/[username]/post/[postId]/_components/post.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-const Post = ({ postId }: { postId: string }) => {
-  return <div>{postId}</div>;
-};
-
-export { Post };
--- web/app/(main)/(user)/u/[username]/post/[postId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Post } from './_components/post';
-
-export default function PostPage({ params }: { params: { postId: string } }) {
-  return <Post postId={params.postId} />;
-}
